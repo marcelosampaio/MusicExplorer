717f15df68368506b59bff4db057e2005ee747f9
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2025-09-26 17:09:23 -0300

    feat: firebase hosting implementation

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/favorites" element={<Favorites />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
