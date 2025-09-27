c833dc9e9dc935d996ff5dbbf092847530f2eaed
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2025-09-27 20:53:16 -0300

    nav bar adjusts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ import "./../styles/NavBar.css";
 function NavBar() {
   return (
     <nav className="nav-bar">
-      <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
-      <Link to="/login" style={{ marginRight: "10px" }}>Login</Link>
-      <Link to="/register" style={{ marginRight: "10px" }}>Registro</Link>
-      <Link to="/profile" style={{ marginRight: "10px" }}>Perfil</Link>
+      <Link className="nav-link" to="/">Home</Link>
+      <Link className="nav-link" to="/login">Login</Link>
+      <Link className="nav-link" to="/register">Registro</Link>
+      <Link className="nav-link" to="/profile">Perfil</Link>
     </nav>
   );
 }
