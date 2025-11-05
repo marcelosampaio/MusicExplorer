b5041516bf811b629a528cc3e5d1c4c960aabc20
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2025-11-05 17:42:45 -0300

    feat: register new user is ok

---
+++ src/models/modeloSignup.js
@@ -0,0 +1,7 @@
+const modeloSignup = {
+  email: { message: "", show: false },
+  password: { message: "", show: false },
+  confirmPassword: { message: "", show: false },
+};
+
+export { modeloSignup };
