63b303fda0966d7e934238a504629e63704d0c78
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2025-11-05 15:58:19 -0300

    wip: fav viewed at profile page

--- src/components/MusicList.js
+++ src/components/MusicList.js
@@ -2,32 +2,21 @@ import React from "react";
 import { Grid } from "@mui/material";
 import MusicCard from "./MusicCard";
 
-function MusicList({ musics, favorites, onToggleFavorite, onPlayPreview }) {
+function MusicList({
+  musics,
+  favorites,
+  loadingFavorites = [],
+  onToggleFavorite,
+  onPlayPreview,
+}) {
   return (
-    <Grid
-      container
-      spacing={{ xs: 2, sm: 3, md: 3 }}
-      sx={{
-        px: { xs: 2, sm: 3, md: 4 },
-        py: { xs: 2, sm: 3, md: 4 },
-        justifyContent: "center",
-      }}
-    >
+    <Grid container spacing={2} justifyContent="center">
       {musics.map((music) => (
-        <Grid
-          item
-          key={music.id}
-          xs={12}
-          sm={6}
-          md={3}
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
+        <Grid item key={music.id}>
           <MusicCard
             music={music}
             isFavorite={favorites.includes(music.id)}
+            isLoading={loadingFavorites.includes(music.id)}
             onToggleFavorite={onToggleFavorite}
             onPlayPreview={onPlayPreview}
           />
