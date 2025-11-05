1ccf150815edbad211540e2ffcf898f9cb23f0aa
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2025-11-05 15:05:39 -0300

    wip: MusicList adjustment

--- src/components/MusicList.js
+++ src/components/MusicList.js
@@ -1,40 +1,40 @@
-import React from "react";
-import { Grid } from "@mui/material";
-import MusicCard from "./MusicCard";
+  import React from "react";
+  import { Grid } from "@mui/material";
+  import MusicCard from "./MusicCard";
 
-function MusicList({ musics, favorites, onToggleFavorite, onPlayPreview }) {
-  return (
-    <Grid
-      container
-      spacing={{ xs: 2, sm: 3, md: 3 }}
-      sx={{
-        px: { xs: 2, sm: 3, md: 4 },
-        py: { xs: 2, sm: 3, md: 4 },
-        justifyContent: "center",
-      }}
-    >
-      {musics.map((music) => (
-        <Grid
-          item
-          key={music.id}
-          xs={12}  // mobile: 1 coluna
-          sm={6}   // tablet: 2 colunas
-          md={3}   // desktop: 4 colunas
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          <MusicCard
-            music={music}
-            isFavorite={favorites.includes(music.id)}
-            onToggleFavorite={onToggleFavorite}
-            onPlayPreview={onPlayPreview}
-          />
-        </Grid>
-      ))}
-    </Grid>
-  );
-}
+  function MusicList({ musics, favorites, onToggleFavorite, onPlayPreview }) {
+    return (
+      <Grid
+        container
+        spacing={{ xs: 2, sm: 3, md: 3 }}
+        sx={{
+          px: { xs: 2, sm: 3, md: 4 },
+          py: { xs: 2, sm: 3, md: 4 },
+          justifyContent: "center",
+        }}
+      >
+        {musics.map((music) => (
+          <Grid
+            item
+            key={music.id}
+            xs={12}  // mobile: 1 coluna
+            sm={6}   // tablet: 2 colunas
+            md={3}   // desktop: 4 colunas
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <MusicCard
+              music={music}
+              isFavorite={favorites.includes(music.id)}
+              onToggleFavorite={onToggleFavorite}
+              onPlayPreview={onPlayPreview}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    );
+  }
 
-export default MusicList;
+  export default MusicList;
