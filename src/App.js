cdd717fb860becf286340138bcea53db482669ea
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2025-11-05 14:47:06 -0300

    wip: removed unused App.css

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { CssBaseline } from "@mui/material";
-import { AuthProvider } from "./contexts/AuthProvider";
-import Header from "./components/Header";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
+  import React from "react";
+  import { Routes, Route, Navigate } from "react-router-dom";
+  import { CssBaseline } from "@mui/material";
+  import { AuthProvider } from "./contexts/AuthProvider";
+  import Header from "./components/Header";
+  import Login from "./pages/Login";
+  import Register from "./pages/Register";
+  import Home from "./pages/Home";
+  import Profile from "./pages/Profile";
 
-function App() {
-  return (
-    <AuthProvider>
-      <CssBaseline />
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </AuthProvider>
-  );
-}
+  function App() {
+    return (
+      <AuthProvider>
+        <CssBaseline />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </AuthProvider>
+    );
+  }
 
-export default App;
+  export default App;
